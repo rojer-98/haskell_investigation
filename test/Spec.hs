80164173b6cebe36edc5088d13c753580a8a7486
rojer-98 <rojermagnuson98@gmail.com>
2023-05-31 10:55:39 +0300

    WP

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+module Main where
+
+import Lib (
+    additionalFunc,
+    ioAggregatior,
+    printAllBinTree,
+    reverseBool,
+    sampleNum,
+    someFunc,
+    testLens,
+ )
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn " [BEGIN]"
+
+simpleSampleNum :: TestTree
+simpleSampleNum = testCase "60" sampleNum
+
+simpleSomeFunc :: TestTree
+simpleSomeFunc = testCase "someFunc" someFunc
+
+simpleAdditionalFunc :: TestTree
+simpleAdditionalFunc = testCase "additionalFunc" additionalFunc
+
+simpleGroup :: TestTree
+simpleGroup = testGroup "simpleTests" [simpleSampleNum, simpleSomeFunc, simpleAdditionalFunc]
