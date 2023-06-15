8548f23b161baf6075beb337a8ea7c099ff86f9b
rojer-98 <rojermagnuson98@gmail.com>
2023-06-15 11:13:12 +0300

    WP

--- src/Template.hs
+++ src/Template.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Template (someSplice, generateTupleClass) where
+module Template (
+    someSplice,
+    generateTupleClass,
+    generateTupleInstance,
+    generateTupleBoilerplate,
+)
+where
 
 import Control.Monad (unless)
 import Data.Traversable (for)
@@ -29,3 +35,40 @@ generateTupleClass size = do
 
     --   _X :: t -> r
     mDecl = SigD methodName (AppT (AppT ArrowT (VarT t)) (VarT r))
+
+generateTupleInstance :: Int -> Int -> Q [Dec]
+generateTupleInstance element size = do
+    unless (size > 0) $
+        fail $
+            "Non-positive size: " ++ element'
+    unless (size >= element) $
+        fail $
+            "Can't extract element " ++ element' ++ " of " ++ size' ++ "-tuple"
+    pure [iDecl]
+  where
+    element' = show element
+    size' = show size
+    className = mkName ("Tuple" ++ element')
+    methodName = mkName ('_' : element')
+
+    x = mkName "x"
+
+    vars = [mkName ('t' : show n) | n <- [1 .. size]]
+
+    signature = foldl (\acc var -> AppT acc (VarT var)) (TupleT size) vars
+
+    -- instance TupleX (t1, ..., tX, ...) tX where
+    iDecl = InstanceD Nothing [] (AppT (AppT (ConT className) signature) (VarT $ mkName ('t' : element'))) [mDecl]
+    --   _X (_, _, ..., x, ...) = x
+    mDecl = FunD methodName [Clause [TupP $ replicate (element - 1) WildP ++ [VarP x] ++ replicate (size - element) WildP] (NormalB $ VarE x) []]
+
+generateTupleBoilerplate :: Int -> Q [Dec]
+generateTupleBoilerplate size =
+    concatFor [1 .. size] $ \classDeclIndex -> do
+        cDecl <- generateTupleClass classDeclIndex
+        iDecls <- for [1 .. classDeclIndex] $ \instanceDeclIndex ->
+            generateTupleInstance instanceDeclIndex classDeclIndex
+
+        pure $ concat (cDecl : iDecls)
+  where
+    concatFor xs = fmap concat . for xs
